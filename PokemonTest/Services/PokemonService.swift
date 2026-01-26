e554bbb3da0468bb2957bc840ddce8e4120ceaab
Jose Bustos <35240264+korbek99@users.noreply.github.com>
2026-01-26 12:44:24 -0300

    adding PokemonViewModel
    adding PokemonViewModel

---
+++ PokemonTest/Services/PokemonService.swift
@@ -0,0 +1,8 @@
+//
+//  PokemonService.swift
+//  PokemonTest
+//
+//  Created by Jose Preatorian on 26-01-26.
+//
+
+import Foundation
---
+++ PokemonTest/ViewModels/PokemonViewModel.swift
@@ -0,0 +1,8 @@
+//
+//  PokemonViewModel.swift
+//  PokemonTest
+//
+//  Created by Jose Preatorian on 26-01-26.
+//
+
+import Foundation
