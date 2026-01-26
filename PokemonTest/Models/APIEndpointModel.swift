7dd6c451ef813a6eefcde1d72a6c0806e290d2d6
Jose Bustos <35240264+korbek99@users.noreply.github.com>
2026-01-26 12:42:24 -0300

    adding endpoints
    adding endpoints

---
+++ PokemonTest/Models/APIEndpointModel.swift
@@ -0,0 +1,14 @@
+//
+//  APIEndpointModel.swift
+//  PokemonTest
+//
+//  Created by Jose Preatorian on 26-01-26.
+//
+
+import Foundation
+
+public struct APIEndpointModel {
+    let url: URL
+    let APIKey: String
+    let APIToken: String?
+}
