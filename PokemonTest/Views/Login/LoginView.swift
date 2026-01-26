0a6f06a321ad7f01daa6393f490df10308af7e3a
Jose Bustos <35240264+korbek99@users.noreply.github.com>
2026-01-26 12:58:24 -0300

    adding LoginView
    adding LoginView

---
+++ PokemonTest/Views/Login/LoginView.swift
@@ -0,0 +1,18 @@
+//
+//  LoginView.swift
+//  PokemonTest
+//
+//  Created by Jose Preatorian on 26-01-26.
+//
+
+import SwiftUI
+
+struct LoginView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    LoginView()
+}
